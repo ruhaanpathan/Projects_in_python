ca0adb77f8eeb8625b457e3f1a5bd1e8094ead77
ruhaanpathan <139214773+ruhaanpathan@users.noreply.github.com>
2024-09-08 19:17:00 +0530

    Update radius.py

--- radius.py
+++ radius.py
@@ -1,3 +1,8 @@
-radius=int(input('the the value of your radius: '))
-answer=3.14*radius*radius
-print('the ans is',answer)
+radius=0
+while(radius==0):
+    try:
+        radius=int(input('the the value of your radius: '))
+        answer=3.14*radius*radius
+        print('the area of circle is',answer)
+    except Exception:
+        print('Please do not enter any letter')
